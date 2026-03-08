509f37eb157b8e82242bc778ababee013c4e4889
Shinn Thant <sthant497@gmail.com>
2026-03-08 09:13:51 +0630

    some changes

--- src/main/java/uniproject/exam/seating/room/roomService.java
+++ src/main/java/uniproject/exam/seating/room/roomService.java
@@ -1,17 +1,22 @@
 package uniproject.exam.seating.room;
 
 import org.springframework.stereotype.Service;
+import uniproject.exam.seating.seating.seatingRepository;
+import uniproject.exam.seating.student.studentRepository;
 
 import java.util.List;
 
 @Service
 public class roomService {
-
-    public roomRepository roomRepository;
-
-    public roomService(roomRepository roomRepository) {
+    public roomService(studentRepository studentRepository, seatingRepository seatingRepository, roomRepository roomRepository) {
+        this.studentRepository = studentRepository;
+        this.seatingRepository = seatingRepository;
         this.roomRepository = roomRepository;
     }
+
+    public studentRepository studentRepository;
+    public seatingRepository seatingRepository;
+    public roomRepository roomRepository;
 
     public List<Room> getAllRooms() {
         return roomRepository.findAll();
@@ -25,8 +30,11 @@ public class roomService {
 
     public void deleteRoomById(Integer roomId) {
         if (roomRepository.existsById(roomId)) {
+            studentRepository.resetStudentByRoom(roomId);
+            seatingRepository.deleteAllByRoom_RoomId(roomId);
             roomRepository.deleteById(roomId);
         }
+
     }
 
     public void updateRoomById(Integer roomId, Integer floor, String roomName, Integer rowCapacity, Integer columnCapacity) {
