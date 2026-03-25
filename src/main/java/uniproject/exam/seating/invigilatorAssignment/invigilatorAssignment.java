94e2468fc6688dc45559712dc72e6953859b2262
Shinn Thant <sthant497@gmail.com>
2026-03-25 19:54:22 +0630

    create invigilatorAssignment entity

---
+++ src/main/java/uniproject/exam/seating/invigilatorAssignment/invigilatorAssignment.java
@@ -0,0 +1,68 @@
+package uniproject.exam.seating.invigilatorAssignment;
+
+import jakarta.persistence.*;
+import uniproject.exam.seating.exam.Exam;
+import uniproject.exam.seating.invigilator.invigilator;
+import uniproject.exam.seating.room.Room;
+
+@Entity
+public class invigilatorAssignment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int AssignmentId;
+
+    @ManyToOne
+    @JoinColumn(name = "examId", nullable = false)
+    private Exam exam;
+
+    @ManyToOne
+    @JoinColumn(name = "roomId", nullable = false)
+    private Room room;
+
+    @ManyToOne
+    @JoinColumn(name = "invigilatorId", nullable = false)
+    private invigilator invigilator;
+
+    public invigilatorAssignment() {}
+
+    public invigilatorAssignment(Exam exam, Room room, invigilator invigilator) {
+        this.exam = exam;
+        this.room = room;
+        this.invigilator = invigilator;
+    }
+
+    public int getAssignmentId() {
+        return AssignmentId;
+    }
+
+    public void setAssignmentId(int assignmentId) {
+        AssignmentId = assignmentId;
+    }
+
+    public Exam getExam() {
+        return exam;
+    }
+
+    public void setExam(Exam exam) {
+        this.exam = exam;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public invigilator getInvigilator() {
+        return invigilator;
+    }
+
+    public void setInvigilator(invigilator invigilator) {
+        this.invigilator = invigilator;
+    }
+}
+
+
+
