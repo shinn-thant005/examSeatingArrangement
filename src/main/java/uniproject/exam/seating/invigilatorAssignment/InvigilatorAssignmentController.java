293d8c23bf53e32c273ca97bc48e4b007b9dcbcc
Shinn Thant <sthant497@gmail.com>
2026-03-25 21:07:10 +0630

    update Assignment Controller

--- src/main/java/uniproject/exam/seating/invigilatorAssignment/InvigilatorAssignmentController.java
+++ src/main/java/uniproject/exam/seating/invigilatorAssignment/InvigilatorAssignmentController.java
@@ -1,22 +1,28 @@
 package uniproject.exam.seating.invigilatorAssignment;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/invigilatorAssignment")
 public class InvigilatorAssignmentController {
-    InvigilatorAssignmentService invigilatorAssignmentService;
+
+    private final InvigilatorAssignmentService invigilatorAssignmentService;
 
     public InvigilatorAssignmentController(InvigilatorAssignmentService invigilatorAssignmentService) {
         this.invigilatorAssignmentService = invigilatorAssignmentService;
     }
 
     @GetMapping("/list-assignment")
-    public List<InvigilatorAssignment> getAllAssignment() {
+    public List<InvigilatorAssignment> listAllAssignment() {
         return invigilatorAssignmentService.getAllAssignment();
     }
 
+    @PostMapping("/generate-assignment/{examId}")
+    public String generateAssignmentPlan(@PathVariable Integer examId) {
+        // Calls the Genetic Algorithm in your service
+        invigilatorAssignmentService.generateAssignment(examId);
+
+        return "Invigilator assignments have been successfully generated for Exam ID: " + examId;
+    }
 }
